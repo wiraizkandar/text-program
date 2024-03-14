f12111ad754157f4c02fbc0f0aa9ee744bec619c
wiraizkandar <wiraizkandar1@gmail.com>
2024-03-14 23:09:47 +0800

    fix typo button text

--- resources/js/pages/PageForm.vue
+++ resources/js/pages/PageForm.vue
@@ -19,7 +19,7 @@
 							type="submit"
 							class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 						>
-							Tranform Text
+							Transform Text
 						</button>
 					</div>
 				</form>
